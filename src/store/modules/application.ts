b7c7d006dca7709dcb7376b0ca089eb8ed8883f0
Miyano <dr000w@gmail.com>
2018-06-10 11:41:47 -0400

    First commit after removing typescript support

---
+++ src/store/modules/application.ts
@@ -0,0 +1,26 @@
+// module application
+// manage the entire application state
+
+enum SidebarType {
+  FULL = 0,
+  MINI,
+  NONE
+}
+
+const state = {
+  sidebarType: SidebarType.FULL
+}
+
+const getters = {}
+
+const actions = {}
+
+const mutations = {}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+}
+
